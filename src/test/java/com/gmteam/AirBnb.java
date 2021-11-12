ffec09dde85560d43762e25e80688ac9017f9dc1
Furki <bsturk.f@gmail.com>
2021-11-12 14:54:12 +0300

    Add Scenario 2

--- src/test/java/com/gmteam/AirBnb.java
+++ src/test/java/com/gmteam/AirBnb.java
@@ -9,7 +9,7 @@ import org.testng.annotations.Test;
 public class AirBnb extends BaseTest {
 
 
-    @Test(enabled = false, description = "Scenario 1 - Login With Email Address")
+    @Test(enabled = true, description = "Scenario 1 - Login With Email Address")
     public void TestScenario_01() throws Exception {
         LoginPage loginPage = new LoginPage();
         MainPage mainPage = new MainPage();
@@ -27,7 +27,15 @@ public class AirBnb extends BaseTest {
     }
 
     @Test(enabled = true, description = "Scenario 2 - Add to Wishlist")
-    public void TestScenario_02() {
+    public void TestScenario_02() throws Exception {
+        MainPage mainPage = new MainPage();
+
+        mainPage.clickExplore();
+        mainPage.clickWhereAreYou();
+        mainPage.setWhereAre();
+        mainPage.clickAntalya();
+        mainPage.clickPlaceToStay();
+        mainPage.swapCalendar();
 
     }
 
